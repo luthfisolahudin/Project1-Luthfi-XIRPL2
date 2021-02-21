a4d402d68236500b3be3476c1ce217bf31d2b4e7
Luthfi Solahudin <54571802+luthfisolahudin@users.noreply.github.com>
2021-02-21 15:10:29 +0700

    Implementasi kelas DaftarMenu

--- src/main/java/id/smkn4/pbo/restaurantCashier/classes/DaftarMenu.java
+++ src/main/java/id/smkn4/pbo/restaurantCashier/classes/DaftarMenu.java
@@ -6,11 +6,30 @@ import java.util.ArrayList;
 public class DaftarMenu {
     private ArrayList<Menu> daftarMenu;
 
-    public DaftarMenu() {}
+    public DaftarMenu() {
+        daftarMenu = new ArrayList<>();
+    }
 
-    public void tambahMenu(Menu menu) {}
+    public void tambahMenu(Menu menu) {
+        daftarMenu.add(menu);
+    }
 
-    public void getMenuByKategori(String kategori) {}
+    public void getMenuByKategori(String kategori) {
+        System.out.println("======== " + kategori + " ========");
 
-    public void tampilkanDaftarMenu() {}
+        for (int i = 0; i < daftarMenu.size(); i++) {
+            Menu m = daftarMenu.get(i);
+
+            if (m.getKategori().equals(kategori))
+                System.out.println(i + 1 + ". " + m.getNama() + "\t" + m.getHarga());
+        }
+    }
+
+    public void tampilkanDaftarMenu() {
+        System.out.println("======== ALDEBARAMEN ========");
+        getMenuByKategori("Ramen");
+        getMenuByKategori("Kuah");
+        getMenuByKategori("Topping");
+        getMenuByKategori("Minuman");
+    }
 }
